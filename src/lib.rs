e9ff39400d6b5b53a1983a591e7c6288be6066c5
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-05-11 21:33:38 -0700

    Allow customization of gain and measurement config

--- src/lib.rs
+++ src/lib.rs
@@ -33,6 +33,56 @@ pub enum Error<CommE, PinE> {
     UnknownChipId,
 }
 
+/// Gain settings
+/// gain in LSb/Gauss
+/// One tesla (T) is equal to 104 gauss,
+#[repr(u8)]
+pub enum GainSetting {
+    ///± 0.88 Ga  / 0.73 (mGa/LSb)
+    Gain1370 = 0b00000000,
+    ///± 1.30 Ga  / 0.92 (mGa/LSb)
+    Gain1090 = 0b00100000,
+    ///± 1.90 Ga  / 1.22 (mGa/LSb)
+    Gain0820 = 0b01000000,
+    ///± 2.50 Ga  / 1.52 (mGa/LSb)
+    Gain0660 = 0b01100000,
+    ///± 4.00 Ga  / 2.27 (mGa/LSb)
+    Gain0440 = 0b10000000,
+    ///± 4.70 Ga  / 2.56 (mGa/LSb)
+    Gain0390 = 0b10100000,
+    ///± 5.60 Ga  / 3.03 (mGa/LSb)
+    Gain0330 = 0b11000000,
+    ///± 8.10 Ga  / 4.35 (mGa/LSb)
+    Gain0230 = 0b11100000,
+}
+
+#[repr(u8)]
+pub enum OdrSetting {
+    Odr_0_75_Hz = 0b000,
+    Odr_1_5_Hz = 0b001,
+    Odr_3_0_Hz = 0b010,
+    Odr_7_5_Hz = 0b011,
+    Odr_15_0_Hz = 0b100,
+    Odr_30_0_Hz = 0b110,
+    Odr_220_0_Hz = 0b111,
+}
+
+#[repr(u8)]
+pub enum SampleAvgSetting {
+    AvgSamples1 = 0b00,
+    AvgSamples2 = 0b01,
+    AvgSamples4 = 0b10,
+    AvgSamples8 = 0b11,
+}
+
+#[repr(u8)]
+pub enum MeasurementModeSetting {
+    NormalMode = 0b00,
+    PositiveBias = 0b01,
+    NegativeBias = 0b10,
+    TemperatureOnly = 0b11,
+}
+
 const REG_CONFIG_A: u8 = 0x00;
 const REG_CONFIG_B: u8 = 0x01;
 const REG_CONFIG_C: u8 = 0x02;
@@ -60,10 +110,8 @@ const REG_ID_C: u8 = 0x0C;
 // const REG_TEMP_OUTPUT_MSB: u8 = 0x31;
 // const REG_TEMP_OUTPUT_LSB: u8 = 0x32;
 
-
 // Status Register 2
 // const REG_STATUS2: u8 = 0x09;
-
 
 const BLOCK_BUF_LEN: usize = 32;
 
@@ -71,6 +119,8 @@ pub struct HMC5983<SI> {
     pub(crate) sensor_interface: SI,
     /// Buffer for reads and writes to the sensor
     block_buf: [u8; BLOCK_BUF_LEN],
+
+
 }
 
 impl<SI, CommE, PinE> HMC5983<SI>
@@ -89,6 +139,7 @@ where
     }
 
     fn reset(&mut self, delay_source: &mut impl DelayMs<u8>) -> Result<(), crate::Error<CommE, PinE>> {
+
         #[cfg(feature = "rttdebug")]
         for reg in 0x00..0x0D {
             let val = self.read_reg(reg)?;
@@ -99,7 +150,6 @@ where
         const EXPECTED_PROD_ID_B: u8 = 52; //'4';
         const EXPECTED_PROD_ID_C: u8 = 51; //'3';
         //compare product ID against known product ID
-
         //read the product identifiers
         self.sensor_interface.read_block(REG_ID_A,&mut self.block_buf[..3])?;
         if self.block_buf[0] != EXPECTED_PROD_ID_A ||
@@ -113,26 +163,51 @@ where
             return Err(Error::UnknownChipId);
         }
 
-        // 1.9 Gauss range -- suggested:  0xA0 (gain 5)
-        let range_config:u8 = 0x40;
-
         // Write CRA (00) – send 0x3C 0x00 0x70 (8-average, 15 Hz default, normal measurement)
-        self.sensor_interface.write_reg(REG_CONFIG_A, 0x70)?;
-        // Set gain
-        self.sensor_interface.write_reg(REG_CONFIG_B, range_config)?;
+        // REG_CONFIG_A can set the Output Data Rate (ODR)
+        //
+
+        self.set_all_config_a(MeasurementModeSetting::NormalMode,
+        OdrSetting::Odr_30_0_Hz,
+            SampleAvgSetting::AvgSamples8,
+            true
+        )?;
+
+        self.set_gain(GainSetting::Gain0820)?;
         // Write Mode (02) – send 0x3C 0x02 0x00 (Continuous-measurement mode)
         self.sensor_interface.write_reg(REG_CONFIG_C, 0x00)?;
         delay_source.delay_ms(100);
 
-        let confirm_val = self.read_reg(REG_CONFIG_B)?;
-        if confirm_val != range_config {
-            #[cfg(feature = "rttdebug")]
-            rprintln!("range bad: expected {} got {}",range_config, confirm_val);
-            return Err(Error::Configuration);
-        }
-
         Ok(())
     }
+
+    pub fn set_gain(&mut self, gain: GainSetting) -> Result<(), crate::Error<CommE, PinE>>  {
+        let gain_val: u8 = gain as u8;
+        self.sensor_interface.write_reg(REG_CONFIG_B, gain_val)?;
+
+        let confirm_val = self.read_reg(REG_CONFIG_B)?;
+        if confirm_val != gain_val {
+            #[cfg(feature = "rttdebug")]
+            rprintln!("gain bad: expected {} got {}",gain_val, confirm_val);
+            return Err(Error::Configuration);
+        }
+        Ok(())
+    }
+
+    pub fn set_all_config_a(&mut self,
+        mode: MeasurementModeSetting,
+        odr: OdrSetting,
+        averaging: SampleAvgSetting,
+        temp_enabled: bool) -> Result<(), crate::Error<CommE, PinE>> {
+
+        let new_val =
+            (if temp_enabled { (1 << 7) } else {0}) &
+                ((averaging as u8) << 6) &
+                ((odr as u8) << 4) &
+                ((mode as u8) << 2);
+        self.sensor_interface.write_reg(REG_CONFIG_A, new_val)
+    }
+
 
 
     /// Read a single register
@@ -143,23 +218,22 @@ where
     }
 
     /// Verify that a magnetometer reading is within the expected range.
-    /// From section "3.4 Magnetic Sensor Specifications" in datasheet
-    fn reading_in_range(sample: &[i16; 3]) -> bool {
-        /// Maximum Dynamic Range for X and Y axes (micro Teslas)
-        const MDR_XY_AXES: i16 = 1600;
-        /// Maximum Dynamic Range for Z axis (micro Teslas)
-        const MDR_Z_AXIS: i16 = 2500;
-        /// Resolution (micro Teslas per LSB)
-        const RESO_PER_BIT: f32 = 0.3;
-        const MAX_VAL_XY: i16 =
-            (((MDR_XY_AXES as f32) / RESO_PER_BIT) as i16) + 1;
-        const MAX_VAL_Z: i16 =
-            (((MDR_Z_AXIS as f32) / RESO_PER_BIT) as i16) + 1;
-
-        sample[0].abs() < MAX_VAL_XY
-            && sample[1].abs() < MAX_VAL_XY
-            && sample[2].abs() < MAX_VAL_Z
-    }
+    // fn reading_in_range(sample: &[i16; 3]) -> bool {
+    //     /// Maximum Dynamic Range for X and Y axes (micro Teslas)
+    //     const MDR_XY_AXES: i16 = 1600;
+    //     /// Maximum Dynamic Range for Z axis (micro Teslas)
+    //     const MDR_Z_AXIS: i16 = 2500;
+    //     /// Resolution (micro Teslas per LSB)
+    //     const RESO_PER_BIT: f32 = 0.3;
+    //     const MAX_VAL_XY: i16 =
+    //         (((MDR_XY_AXES as f32) / RESO_PER_BIT) as i16) + 1;
+    //     const MAX_VAL_Z: i16 =
+    //         (((MDR_Z_AXIS as f32) / RESO_PER_BIT) as i16) + 1;
+    //
+    //     sample[0].abs() < MAX_VAL_XY
+    //         && sample[1].abs() < MAX_VAL_XY
+    //         && sample[2].abs() < MAX_VAL_Z
+    // }
 
     /// Combine high and low bytes of i16 mag value
     fn raw_reading_to_i16(buf: &[u8], idx: usize) -> i16 {
@@ -183,12 +257,12 @@ where
             Self::raw_reading_to_i16(&self.block_buf, 4),
         ];
 
-        if !Self::reading_in_range(&sample_i16) {
-            #[cfg(feature = "rttdebug")]
-            rprintln!("bad reading?");
-
-            return Err(Error::OutOfRange);
-        }
+        // if !Self::reading_in_range(&sample_i16) {
+        //     #[cfg(feature = "rttdebug")]
+        //     rprintln!("bad reading?");
+        //
+        //     return Err(Error::OutOfRange);
+        // }
 
         //TODO do cross-axis flow calibration?
         Ok(sample_i16)
